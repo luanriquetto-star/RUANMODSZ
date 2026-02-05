129aad46906c86c5b9fe3da1ab6725d6f61be6a3
luanriquetto-star <luanriquetto@gmail.com>
2026-02-05 10:25:54 -0300

    script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,9 @@
-RUANMODSZ
-Script RUAN MODSZ
+-- RUAN MODSZ | Script Teste
+
+print("RUAN MODSZ CARREGADO COM SUCESSO")
+
+game.StarterGui:SetCore("SendNotification", {
+	Title = "RUAN MODSZ",
+	Text = "Script executado com sucesso!",
+	Duration = 5
+})
